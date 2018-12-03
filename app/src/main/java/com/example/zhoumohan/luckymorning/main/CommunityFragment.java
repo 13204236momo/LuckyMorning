f4b191e707974e22a20b7ed5963a6fdd1dadcc46
mac_home <953605663@qq.com>
2018-12-03 23:11:13 +0800

    添加fab

--- app/src/main/java/com/example/zhoumohan/luckymorning/main/CommunityFragment.java
+++ app/src/main/java/com/example/zhoumohan/luckymorning/main/CommunityFragment.java
@@ -3,6 +3,7 @@ package com.example.zhoumohan.luckymorning.main;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -16,11 +17,15 @@ import com.example.zhoumohan.luckymorning.community.CommunityDetailMvpFragment;
 import com.flyco.tablayout.CommonTabLayout;
 import com.flyco.tablayout.listener.CustomTabEntity;
 import com.flyco.tablayout.listener.OnTabSelectListener;
+import com.gordonwong.materialsheetfab.DimOverlayFrameLayout;
+import com.gordonwong.materialsheetfab.MaterialSheetFab;
+import com.gordonwong.materialsheetfab.MaterialSheetFabEventListener;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class CommunityFragment extends BaseFragment {
 
@@ -28,6 +33,10 @@ public class CommunityFragment extends BaseFragment {
     CommonTabLayout tlCommunity;
     @BindView(R.id.vp_community)
     ViewPager vpCommunity;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.overlay)
+    DimOverlayFrameLayout overlay;
 
     private String[] mTitles = {"动态", "任务", "关注"};
     private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
@@ -36,6 +45,8 @@ public class CommunityFragment extends BaseFragment {
     private NewsFragment3 newsFragment3;
     private NewsFragment4 newsFragment4;
     private ArrayList<Fragment> mFragments = new ArrayList<>();
+
+    private MaterialSheetFab materialSheetFab;
 
 
     @Override
@@ -47,30 +58,16 @@ public class CommunityFragment extends BaseFragment {
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
         ButterKnife.bind(this, view);
+
         communityDetailMvpFragment = new CommunityDetailMvpFragment();
         newsFragment3 = new NewsFragment3();
         newsFragment4 = new NewsFragment4();
         mFragments.add(communityDetailMvpFragment);
         mFragments.add(newsFragment3);
         mFragments.add(newsFragment4);
+
         vpCommunity.setAdapter(new MyPagerAdapter(getActivity().getSupportFragmentManager()));
         vpCommunity.setCurrentItem(0);
-        vpCommunity.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-              tlCommunity.setCurrentTab(position);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
 
         if (mTabEntities.size() == 0) {
             for (int i = 0; i < mTitles.length; i++) {
@@ -79,6 +76,50 @@ public class CommunityFragment extends BaseFragment {
         }
 
         tlCommunity.setTabData(mTabEntities);
+
+        int sheetColor = getResources().getColor(R.color.background_card);
+        int fabColor = getResources().getColor(R.color.theme_accent);
+
+        // Create material sheet FAB
+        materialSheetFab = new MaterialSheetFab<>(fab, sheetView, overlay, sheetColor, fabColor);
+
+        // Set material sheet event listener
+        materialSheetFab.setEventListener(new MaterialSheetFabEventListener() {
+            @Override
+            public void onShowSheet() {
+
+            }
+
+            @Override
+            public void onHideSheet() {
+
+            }
+        });
+
+
+        initEvent();
+
+    }
+
+    private void initEvent(){
+        vpCommunity.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                tlCommunity.setCurrentTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
         tlCommunity.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelect(int position) {
@@ -90,7 +131,26 @@ public class CommunityFragment extends BaseFragment {
 
             }
         });
+
     }
+
+    @OnClick({R.id.fab_sheet_item_word,R.id.fab_sheet_item_photo,R.id.fab_sheet_item_audio,R.id.fab_sheet_item_task})
+    void onClick(View view){
+        switch (view.getId()){
+            case R.id.fab_sheet_item_word:
+                break;
+            case R.id.fab_sheet_item_photo:
+                break;
+            case R.id.fab_sheet_item_audio:
+                break;
+            case R.id.fab_sheet_item_task:
+                break;
+
+        }
+        materialSheetFab.hideSheet();
+    }
+
+
 
     private class MyPagerAdapter extends FragmentPagerAdapter {
         public MyPagerAdapter(FragmentManager fm) {
