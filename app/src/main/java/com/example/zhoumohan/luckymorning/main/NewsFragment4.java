8fdbba2a7fb7a9b5035d4830e17d708fbfbe32c9
xiaomo <953605663@qq.com>
2019-03-19 17:28:32 +0800

    添加二维码扫描

--- app/src/main/java/com/example/zhoumohan/luckymorning/main/NewsFragment4.java
+++ app/src/main/java/com/example/zhoumohan/luckymorning/main/NewsFragment4.java
@@ -1,27 +1,82 @@
 package com.example.zhoumohan.luckymorning.main;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.zhoumohan.luckymorning.R;
 import com.example.zhoumohan.luckymorning.base.BaseFragment;
+import com.example.zhoumohan.luckymorning.camera.CaptureActivity;
+import com.example.zhoumohan.luckymorning.util.CameraUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.app.Activity.RESULT_OK;
 
 public class NewsFragment4 extends BaseFragment {
 
+    @BindView(R.id.iv_qr)
+    ImageView ivQr;
 
+    private static final int REQUEST_SCAN = 0;
     @Override
     protected int getLayoutResID() {
-        return R.layout.item_vp;
+        return R.layout.item_vp1;
     }
 
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
+        ButterKnife.bind(this,view);
+    }
 
+
+    @OnClick(R.id.iv_qr)
+    void onClick(View view){
+        switch (view.getId()){
+            case R.id.iv_qr:
+                getRuntimePermission();
+                break;
+        }
+
+    }
+
+    // 获得运行时权限
+    private void getRuntimePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] perms = {Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+            if (getActivity().checkSelfPermission(perms[0]) == PackageManager.PERMISSION_DENIED ||
+                    getActivity().checkSelfPermission(perms[1]) == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(perms, 200);
+            } else {
+                jumpScanPage();
+            }
+        }
+    }
+
+    // 跳转到扫码页
+    private void jumpScanPage() {
+        startActivityForResult(new Intent(getActivity(), CaptureActivity.class), REQUEST_SCAN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SCAN && resultCode == RESULT_OK) {
+            Toast.makeText(getActivity(), data.getStringExtra(CameraUtil.BAR_CODE), Toast.LENGTH_LONG).show();
+        }
     }
 
 
--- app/src/main/java/com/example/zhoumohan/luckymorning/util/Helper.java
+++ app/src/main/java/com/example/zhoumohan/luckymorning/util/Helper.java
@@ -1,5 +1,6 @@
 package com.example.zhoumohan.luckymorning.util;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.Log;
@@ -8,6 +9,7 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
@@ -51,15 +53,17 @@ public class Helper {
     /**
      * 获取状态栏高度
      * */
-    public static int getStatusBarHeight() {
-        int statusBarHeight1 = -1;
-        //获取status_bar_height资源的ID
-        int resourceId = mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            //根据资源ID获取响应的尺寸值
-            statusBarHeight1 = mContext.getResources().getDimensionPixelSize(resourceId);
+    public static int getStatusBarHeight(Context context) {
+        try {
+            @SuppressLint("PrivateApi") Class<?> c = Class.forName("com.android.internal.R$dimen");
+            Object obj = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = Integer.parseInt(field.get(obj).toString());
+            return context.getResources().getDimensionPixelSize(x);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return statusBarHeight1;
+        return 0;
     }
 
     public static int dip2px(Context context, float dipValue) {
