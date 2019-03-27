c158ac0f706297f8a88d75f9cdba054f99f2f1b7
xiaomo <953605663@qq.com>
2019-03-27 16:56:40 +0800

    拖动气泡组件编写

--- app/src/main/java/com/example/zhoumohan/luckymorning/common/widget/bubble/DragBubbleView.java
+++ app/src/main/java/com/example/zhoumohan/luckymorning/common/widget/bubble/DragBubbleView.java
@@ -1,9 +1,11 @@
 package com.example.zhoumohan.luckymorning.common.widget.bubble;
 
+import android.animation.TypeEvaluator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -21,11 +23,14 @@ public class DragBubbleView extends View {
     private int BUBBLE_STATE_DISSMIS = 4;
 
     /**
+     * 两小球最大连接距离
+     */
+    private float MAX_CONNECT_DISTANCE = 60;
+
+    /**
      * 当前状态
      */
     private int currentBubbleState = 1;
-
-
     /**
      * 气泡画笔
      */
@@ -34,6 +39,11 @@ public class DragBubbleView extends View {
      * 文字画笔
      */
     private Paint textPaint;
+
+    /**
+     * 路径画笔
+     */
+    private Paint pathPaint;
 
     /**
      * 静止气泡
@@ -47,12 +57,12 @@ public class DragBubbleView extends View {
     /**
      * 静止气泡半径
      */
-    private float fixBubbleR = 15f;
+    private float fixBubbleR = 12f;
 
     /**
      * 移动气泡半径
      */
-    private float moveBubbleR = 15f;
+    private float moveBubbleR = 12f;
 
     /**
      * 气泡文字
@@ -64,14 +74,18 @@ public class DragBubbleView extends View {
      */
     private Rect textRect;
 
+    /**
+     * A、B、C、D 四点闭合路径
+     */
+    private Path path;
 
 
     public DragBubbleView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public DragBubbleView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public DragBubbleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -88,38 +102,120 @@ public class DragBubbleView extends View {
         textPaint.setColor(Color.WHITE);
         textRect = new Rect();
 
+        pathPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pathPaint.setColor(Color.RED);
+        pathPaint.setStyle(Paint.Style.FILL);
+
+        path = new Path();
+
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
 
-        fixDragBubble = new DragBubble(w/2,h/2,fixBubbleR);
-        moveDragBubble = new DragBubble(w/2,h/2,moveBubbleR);
+        fixDragBubble = new DragBubble(w / 2, h / 2, fixBubbleR);
+        moveDragBubble = new DragBubble(w / 2, h / 2, moveBubbleR);
 
     }
 
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveDragBubble.mCenterX = event.getX();
+                moveDragBubble.mCenterY = event.getY();
+                if (Math.hypot(event.getX() - fixDragBubble.mCenterX, event.getY() - fixDragBubble.mCenterY) > MAX_CONNECT_DISTANCE) {
+                    currentBubbleState = BUBBLE_STATE_INSULATE;
+                } else {
+                    currentBubbleState = BUBBLE_STATE_CONNECT;
+                    fixDragBubble.mRadius = moveDragBubble.mRadius - MAX_CONNECT_DISTANCE/8;
+                }
+
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if (currentBubbleState == BUBBLE_STATE_DEFAULT){
+        if (currentBubbleState == BUBBLE_STATE_DEFAULT) {
             //1.静止状态 气泡和文字
-            canvas.drawCircle(fixDragBubble.mCenterX, fixDragBubble.mCenterY, fixDragBubble.mRadius,bubblePaint);
-            textPaint.getTextBounds(text,0,text.length(),textRect);
-            canvas.drawText(text,fixDragBubble.mCenterX - textRect.width()/2,fixDragBubble.mCenterY+textRect.height()/2,textPaint);
+            canvas.drawCircle(fixDragBubble.mCenterX, fixDragBubble.mCenterY, fixDragBubble.mRadius, bubblePaint);
+            textPaint.getTextBounds(text, 0, text.length(), textRect);
+            canvas.drawText(text, fixDragBubble.mCenterX - textRect.width() / 2, fixDragBubble.mCenterY + textRect.height() / 2, textPaint);
         }
 
         //2.连接状态 静止状态气泡、移动气泡、文字、两气泡和贝塞尔曲线围成的区域
 
+        if (currentBubbleState == BUBBLE_STATE_CONNECT) {
+            //静止气泡
+            canvas.drawCircle(fixDragBubble.mCenterX, fixDragBubble.mCenterY, fixDragBubble.mRadius, bubblePaint);
+            //移动气泡
+            canvas.drawCircle(moveDragBubble.mCenterX, moveDragBubble.mCenterY, moveDragBubble.mRadius, bubblePaint);
+            textPaint.getTextBounds(text, 0, text.length(), textRect);
+            canvas.drawText(text, moveDragBubble.mCenterX - textRect.width() / 2, moveDragBubble.mCenterY + textRect.height() / 2, textPaint);
+
+            //两圆心之间的距离
+            float mDistance = (float) Math.hypot((moveDragBubble.mCenterX - fixDragBubble.mCenterX),(moveDragBubble.mCenterY - fixDragBubble.mCenterY));
+            //控制点坐标
+            float iAnchorX = (moveDragBubble.mCenterX + fixDragBubble.mCenterX) / 2;
+            float iAnchorY = (moveDragBubble.mCenterY + fixDragBubble.mCenterY) / 2;
+
+            float sinTheta = (moveDragBubble.mCenterY - fixDragBubble.mCenterY) / mDistance;
+            float cosTheta = (moveDragBubble.mCenterX - fixDragBubble.mCenterX) / mDistance;
+
+            //A点
+            float aX = fixDragBubble.mCenterX + cosTheta * fixDragBubble.mRadius;
+            float aY = fixDragBubble.mCenterY - sinTheta * fixDragBubble.mRadius;
+
+            //B点
+            float bX = fixDragBubble.mCenterX - cosTheta * fixDragBubble.mRadius;
+            float bY = fixDragBubble.mCenterY + sinTheta * fixDragBubble.mRadius;
+
+            //C点
+            float cX = moveDragBubble.mCenterX + cosTheta * moveDragBubble.mRadius;
+            float cY = moveDragBubble.mCenterY - sinTheta * moveDragBubble.mRadius;
+
+            //D点
+            float dX = moveDragBubble.mCenterX - cosTheta * moveDragBubble.mRadius;
+            float dY = moveDragBubble.mCenterY + sinTheta * moveDragBubble.mRadius;
+
+            path.reset();
+            path.moveTo(cX,cY);
+            path.quadTo(iAnchorX,iAnchorY,aX,aY);
+            path.lineTo(bX,bY);
+            path.quadTo(iAnchorX,iAnchorY,dX,dY);
+            path.close();
+
+            canvas.drawPath(path,pathPaint);
+
+        }
         //3.分离状态  气泡和文字
 
+        if (currentBubbleState == BUBBLE_STATE_INSULATE) {
+            canvas.drawCircle(moveDragBubble.mCenterX, moveDragBubble.mCenterY, moveDragBubble.mRadius, bubblePaint);
+            textPaint.getTextBounds(text, 0, text.length(), textRect);
+            canvas.drawText(text, moveDragBubble.mCenterX - textRect.width() / 2, moveDragBubble.mCenterY + textRect.height() / 2, textPaint);
+        }
+
         //4.消失状态 爆炸效果
+    }
+
+
+    private class RadiuTypeEvaluator implements TypeEvaluator<Integer> {
+
+        @Override
+        public Integer evaluate(float fraction, Integer startValue, Integer endValue) {
+            return null;
+        }
     }
 }
