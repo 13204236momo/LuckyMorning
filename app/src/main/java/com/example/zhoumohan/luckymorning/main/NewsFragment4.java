30b3e646c9697a23bc63a98867514e8258c140f9
mac_home <953605663@qq.com>
2019-04-20 15:15:23 +0800

    底部弹出dialog

--- app/src/main/java/com/example/zhoumohan/luckymorning/main/NewsFragment4.java
+++ app/src/main/java/com/example/zhoumohan/luckymorning/main/NewsFragment4.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import com.example.zhoumohan.luckymorning.R;
 import com.example.zhoumohan.luckymorning.base.BaseFragment;
 import com.example.zhoumohan.luckymorning.camera.CaptureActivity;
+import com.example.zhoumohan.luckymorning.common.widget.BottomDialog;
 import com.example.zhoumohan.luckymorning.demo.MapViewActivity;
 import com.example.zhoumohan.luckymorning.demo.QQBubbleActivity;
 import com.example.zhoumohan.luckymorning.util.CameraUtil;
@@ -54,7 +55,9 @@ public class NewsFragment4 extends BaseFragment {
                 startActivity(new Intent(getActivity(), QQBubbleActivity.class));
                 break;
             case R.id.btn_map:
-                startActivity(new Intent(getActivity(), MapViewActivity.class));
+                //startActivity(new Intent(getActivity(), MapViewActivity.class));
+                BottomDialog dialog = new BottomDialog(getContext(),R.style.BottomDialog);
+                dialog.show();
                 break;
         }
 
---
+++ app/src/main/java/com/example/zhoumohan/luckymorning/util/DisplayUtil.java
@@ -0,0 +1,64 @@
+package com.example.zhoumohan.luckymorning.util;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+
+public class DisplayUtil {
+    /**
+     * 在有虚拟按键的设备中，获取屏幕的宽高是由区别的。这个获得不包括虚拟按键的屏幕。
+     * @param context
+     * @return
+     */
+    public static int [] getScreenRelatedInformation(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        if (windowManager != null) {
+            DisplayMetrics outMetrics = new DisplayMetrics();
+            windowManager.getDefaultDisplay().getMetrics(outMetrics);
+            int widthPixels = outMetrics.widthPixels;
+            int heightPixels = outMetrics.heightPixels;
+            int densityDpi = outMetrics.densityDpi;
+            float density = outMetrics.density;
+            float scaledDensity = outMetrics.scaledDensity;
+            //可用显示大小的绝对宽度（以像素为单位）。
+            //可用显示大小的绝对高度（以像素为单位）。
+            //屏幕密度表示为每英寸点数。
+            //显示器的逻辑密度。
+            //显示屏上显示的字体缩放系数。
+            Log.d("display", "widthPixels = " + widthPixels + ",heightPixels = " + heightPixels + "\n" +
+                    ",densityDpi = " + densityDpi + "\n" +
+                    ",density = " + density + ",scaledDensity = " + scaledDensity);
+            return new int[]{widthPixels,heightPixels};
+        }
+        return null;
+    }
+
+    /**
+     * 在有虚拟按键的设备中，获取屏幕的宽高是由区别的。这个获得包括虚拟按键的屏幕。
+     * @param context
+     * @return
+     */
+    public static int[] getRealScreenRelatedInformation(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        if (windowManager != null) {
+            DisplayMetrics outMetrics = new DisplayMetrics();
+            windowManager.getDefaultDisplay().getRealMetrics(outMetrics);
+            int widthPixels = outMetrics.widthPixels;
+            int heightPixels = outMetrics.heightPixels;
+            int densityDpi = outMetrics.densityDpi;
+            float density = outMetrics.density;
+            float scaledDensity = outMetrics.scaledDensity;
+            //可用显示大小的绝对宽度（以像素为单位）。
+            //可用显示大小的绝对高度（以像素为单位）。
+            //屏幕密度表示为每英寸点数。
+            //显示器的逻辑密度。
+            //显示屏上显示的字体缩放系数。
+            Log.d("display", "widthPixels = " + widthPixels + ",heightPixels = " + heightPixels + "\n" +
+                    ",densityDpi = " + densityDpi + "\n" +
+                    ",density = " + density + ",scaledDensity = " + scaledDensity);
+            return new int[]{widthPixels,heightPixels};
+        }
+        return null;
+    }
+}
